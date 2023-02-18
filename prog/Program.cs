e4c3e99d10ae91111b81421d8d11b5c34d2d32db
Dmitry Bober <bober113d@gmail.com>
2023-02-18 15:51:29 +0500

    добавлен цикл

--- prog/Program.cs
+++ prog/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿string[] array1 = new string[6] {"715", "13", "usa", "11", "result", "name"};
+string[] array2 = new string[array1.Length];
+void SecondArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
